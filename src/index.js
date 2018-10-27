c62723b299f8db60c922a45e97da6955913b8862
Sergii Grebeniuk <sergii.g@preply.com>
2018-10-27 15:22:24 +0300

    Render DOM with ReactJS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-const h1 = document.createElement('h1');
+ReactDOM.render(
+	<div>
+		<h1>My First Heading</h1>
+		<p className="red">My first paragraph</p>
+	</div>,
+	document.body
+);
 
-h1.innerText = 'My First Heading';
-document.body.appendChild(h1);
-
-const p = document.createElement('p');
-p.className = 'red';
-p.innerText = 'My first paragraph';
-document.body.appendChild(p);

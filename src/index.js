1b27115097dd7ac6bef1e8f85f768e5ffba9a198
Sergii Grebeniuk <sergii.g@preply.com>
2018-10-27 14:12:31 +0300

    Add DOM with JS

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const h1 = document.createElement('h1');
+
+h1.innerText = 'My First Heading';
+document.body.appendChild(h1);
+
+const p = document.createElement('p');
+p.className = 'red';
+p.innerText = 'My first paragraph';
+document.body.appendChild(p);
